4731193f90c7f1ece7ce4891bfad3ba136319eb9
hexiaominitao <a8013heming@gmail.com>
2020-04-28 19:09:49 +0800

    cors

--- src/config/index.js
+++ src/config/index.js
@@ -18,7 +18,7 @@ export default {
    */
   baseUrl: {
     dev: 'http://192.168.1.34:5000/api/',
-    pro: 'http://192.168.1.103:5002/api/'
+    pro: 'http://192.168.1.34:5000/api/'
   },
   UploadUrl: {
     dev: 'http://192.168.1.34:5000/api/upload/',

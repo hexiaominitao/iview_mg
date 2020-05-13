03783f9da8f1ae5b7d79176d5d21f7e9e1c7b30f
hexiaominitao <a8013heming@gmail.com>
2020-05-13 17:22:26 +0800

    添加白名单

---
+++ src/view/report/mutation/mutation-w-list.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <Table height='620' border :columns='w_list_title' :data='w_list'></Table>
+  </div>
+</template>
+<script>
+import { getQCAndRaw } from '@/api/data'
+export default {
+  name: 'mutation_w_list',
+  data () {
+    return {
+      w_list_title: [],
+      w_list: []
+    }
+  },
+  methods: {
+    getWList () {
+      const id = this.$route.params.name
+      getQCAndRaw(id).then(res => {
+        this.w_list_title = res.data.w_list_title
+        this.w_list = res.data.w_list
+      })
+    }
+  },
+  mounted () {
+    this.getWList()
+  }
+}
+</script>
